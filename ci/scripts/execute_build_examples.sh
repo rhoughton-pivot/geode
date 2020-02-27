6751415bd459e11ffa301c46f843bf6526a71cdb
Robert Houghton <rhoughton@pivotal.io>
2020-02-27 11:13:22 -0800

    working on making examples run in main pipeline

--- ci/scripts/execute_build_examples.sh
+++ ci/scripts/execute_build_examples.sh
@@ -46,9 +46,18 @@ INSTANCE_IP_ADDRESS="$(cat instance-data/instance-ip-address)"
 
 SET_JAVA_HOME="export JAVA_HOME=/usr/lib/jvm/java-${JAVA_BUILD_VERSION}-openjdk-amd64"
 
+# First build and publish Geode locally
+GRADLE_COMMAND="./gradlew \
+    ${DEFAULT_GRADLE_TASK_OPTIONS} \
+    publishToMavenLocal"
+
+echo "${GRADLE_COMMAND}"
+ssh ${SSH_OPTIONS} geode@${INSTANCE_IP_ADDRESS} "set -x  && mkdir -p tmp && cd geode && ${SET_JAVA_HOME} && ${GRADLE_COMMAND}"
+
+# Then build geode-examples, linking against the locally published jars.
 GRADLE_COMMAND="./gradlew \
     ${DEFAULT_GRADLE_TASK_OPTIONS} \
     clean runAll"
 
 echo "${GRADLE_COMMAND}"
-ssh ${SSH_OPTIONS} geode@${INSTANCE_IP_ADDRESS} "mkdir -p tmp && cd geode && ${SET_JAVA_HOME} && ${GRADLE_COMMAND}"
+ssh ${SSH_OPTIONS} geode@${INSTANCE_IP_ADDRESS} "set -x  && mkdir -p tmp && cd geode-examples && ${SET_JAVA_HOME} && ${GRADLE_COMMAND}"
