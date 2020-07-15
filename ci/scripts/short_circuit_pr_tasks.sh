76f7962042f88664027ab473d86f08ee4b29ebf6
Robert Houghton <rhoughton@pivotal.io>
2020-07-15 14:28:44 -0700

    functional 'is_source_from_pr_testable'

--- ci/scripts/short_circuit_pr_tasks.sh
+++ ci/scripts/short_circuit_pr_tasks.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 
-exit_zero_if_source_from_pr() {
-  set -x
+is_source_from_pr_testable() {
   base_dir=$(git rev-parse --show-toplevel)
   github_pr_dir="${base_dir}/.git/resource"
+  exclude_dirs="${@:-ci}"
+  for d in $(echo ${exclude_dirs}); do
+    local exclude_pathspec="${exclude_pathspec} :(exclude,glob)${d}/**"
+  done
+  local return_code=0
   if [ -d "${github_pr_dir}" ]; then
-    files=$(git diff --name-only $(cat "${github_pr_dir}/base_sha") $(cat"${github_pr_dir}/head_sha"))
-    for f in $files; do
-      # is in ci dir
-      find "${base_dir}" -path "ci" -prune -name "${f}"
-    done
+#    shopt -s extglob
+    pushd ${base_dir} &> /dev/null
+      local files=$(git diff --name-only $(cat "${github_pr_dir}/base_sha") $(cat "${github_pr_dir}/head_sha") -- . $(echo ${exclude_pathspec}))
+    popd &> /dev/null
+    if [[ -z "${files}" ]]; then
+      echo "Code changes are from CI only"
+      return_code=1
+    else
+      echo "real code change here!"
+    fi
   else
     echo "repo is not from a PR"
   fi
-  set +x
+  return ${return_code}
 }
